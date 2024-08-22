d631b4a390b938e10ab5474e35c0389244c769d0
Beniel Thileepan <Beniel.Thileepan@warwick.ac.uk>
2024-08-22 15:54:29 +0300

    Added text labels to poission2d_throughput_proffile.py

--- ops_fpga/poisson2d_througput_profile.py
+++ ops_fpga/poisson2d_througput_profile.py
@@ -28,6 +28,7 @@ time_divisor = 1000
 xticks = np.arange((min(min_300_num_elements, min_400_num_elements)/10000 - 1) * 10000, (max(max_300_num_elements, max_400_num_elements)/10000 + 2) * 10000, 10000)
 print(xticks)
 u280_300_x = query_300_df["num_elements"]
+u280_300_grid = query_300_df["grid_size"]
 u280_p54_300_y = query_300_df["codegen_u280_p54_throughput"]
 u280_p60_300_y = query_300_df["codegen_u280_p60_throughput"]
 u280_400_x = query_400_df["num_elements"]
@@ -58,13 +59,13 @@ fig, ax = plt.subplots()
 # bar3=ax.bar(x+0.4, hand_vck5000, color='white', width=0.2, hatch='//', label='hand_vck5000', edgecolor='#90c46e')
 # bar3=ax.bar(x+0.4, hand_vck5000, color='none', width=0.2, edgecolor='black', **props)
 # ax2 = ax.twinx()
-line1 = ax.plot(u280_300_x[1:], u280_p54_300_y[1:], label="54PE x=300")
-line2 = ax.plot(u280_300_x[1:], u280_p60_300_y[1:], label="60PE x=300")
-line3 = ax.plot(u280_400_x, u280_p54_400_y, label="54PE x=400")
-line4 = ax.plot(u280_400_x, u280_p60_400_y, label="60PE x=400")
+line1 = ax.plot(u280_300_x[1:], u280_p54_300_y[1:], '-o', label="54PE x=300")
+line2 = ax.plot(u280_300_x[1:], u280_p60_300_y[1:], '-o', label="60PE x=300")
+line3 = ax.plot(u280_400_x, u280_p54_400_y, '-o', label="54PE x=400")
+line4 = ax.plot(u280_400_x, u280_p60_400_y, '-o', label="60PE x=400")
 # line2 = ax2.plot(x+0.25, vck5000_imp, color='#70915a', label="vck5000 Imp%")
 
-ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+ax.yaxis.set_major_formatter(FormatStrFormatter('%.0f'))
 ax.xaxis.set_major_locator(MaxNLocator(integer=True))
 # ax.xaxis.set_major_formatter(FormatStrFormatter('%.0f'))
 # ax2.yaxis.set_major_formatter(FormatStrFormatter('%.0f'))
@@ -76,18 +77,20 @@ plt.xticks(xticks)
 
 
 
-# # for ind, val in enumerate(hand_u280):
-# #        ax.text(x[ind], val + 4 , "{:.2f}".format(val), ha='center', size=10, rotation=90) 
+for ind, val in enumerate(u280_p54_300_y[1:].to_list()):
+       ax.text(u280_300_x[1:].to_list()[ind], val + (2 if ind%2 == 0 else -6), "{:.1f}".format(val), ha='center', size=10) 
+    #    ax.text(u280_300_x[1:].to_list()[ind] + 2000, val + 2 , f"(y={u280_300_grid[1:].to_list()[ind][4:]})", ha='center', size=10, rotation=90)
 
-# # for ind, val in enumerate(cgen_u280):
-# #        ax.text(x[ind]+0.2, val + 4 , "{:.2f}".format(val), ha='center', size=10, rotation=90) 
+for ind, val in enumerate(u280_p60_300_y[1:].to_list()):
+        ax.text(u280_300_x[1:].to_list()[ind], val + (2 if ind%2 == 0 else -6), "{:.1f}".format(val), ha='center', size=10) 
 
-# # for ind, val in enumerate(hand_vck5000):
-# #        ax.text(x[ind]+0.4, val + 4 , "{:.2f}".format(val), ha='center', size=10, rotation=90) 
+for ind, val in enumerate(u280_p54_400_y.to_list()):
+        ax.text(u280_400_x.to_list()[ind], val + 2, "{:.1f}".format(val), ha='center', size=10) 
 
-# # for ind, val in enumerate(cgen_vck5000):
-# #        ax.text(x[ind]+0.6, val + 4 , "{:.2f}".format(val), ha='center', size=10, rotation=90) 
-       
+for ind, val in enumerate(u280_p60_400_y.to_list()):
+        ax.text(u280_400_x.to_list()[ind], val + 2, "{:.1f}".format(val), ha='center', size=10) 
+
+
 # ax.set_yscale('log')
 ax.set_axisbelow(True)
 ax.grid(which='both', axis='y', linewidth=1, alpha=0.5)
@@ -98,6 +101,7 @@ handles, labels = ax.get_legend_handles_labels()
 ax.legend(handles, labels, loc=2, ncol=3, facecolor='w', framealpha=1, edgecolor='black', prop={'size': 12})
 ax.set_xlabel('Number of Elements (Thousands)')
 ax.set_ylabel('Throughput (GOps/s)')
+
 
 ax.set_ylim([625, 825])
 # ax2.set_ylim([-20,60])
