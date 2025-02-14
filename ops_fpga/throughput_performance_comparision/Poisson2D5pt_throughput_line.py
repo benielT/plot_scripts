db5472a6eec469186a3e0d2e8f93a025577ce503
Beniel Thileepan <Beniel.Thileepan@warwick.ac.uk>
2025-02-14 12:38:06 +0300

    Throughput line plots updated to in-corporate updated data

--- ops_fpga/throughput_performance_comparision/Poisson2D5pt_throughput_line.py
+++ ops_fpga/throughput_performance_comparision/Poisson2D5pt_throughput_line.py
@@ -3,7 +3,7 @@ import numpy as np
 from matplotlib.ticker import FormatStrFormatter
 import pandas as pd
 from pandas.plotting import table
-
+from lineplot_params import general_font_size, power_marker_size, throughput_marker_size, label_font_size, legends_font_size
 
 #Color Palette
 colors = ['#003f5c', '#58508d', '#bc5090', '#ff6361', '#ffa600', '#34a853'] #['#4e79a7', '#f28e2b', '#e15759', '#76b7b2', '#59a14f', '#edc948'] 
@@ -25,23 +25,23 @@ h100_100b = df["H100_100B"]
 
 # Configure plot settings
 plt.rcParams["figure.figsize"] = (9, 5.5)
-plt.rcParams.update({'font.size': 12})
+plt.rcParams.update({'font.size': general_font_size})
 
 fig, ax = plt.subplots()
 
 # Add secondary y-axis for power usage
 ax2 = ax.twinx()
-ax2.plot(xticks, cgen_u280_power, linestyle='dashdot', marker='s', markersize=10, label="u280 power", color='#6d65a3', markeredgecolor='#000000')#'#a59fd1')#'#f7b16a')
-ax2.plot(xticks, h100_power, linestyle='dashdot', marker='s', markersize=10, label="H100 power", color='#4dc46d', markeredgecolor='#000000')#'#92f0ab')#'#f5dc84')
+ax2.plot(xticks, cgen_u280_power, linestyle='dashdot', marker='s', markersize=power_marker_size, label="U280 power", color='#6d65a3', markeredgecolor='#000000')#'#a59fd1')#'#f7b16a')
+ax2.plot(xticks, h100_power, linestyle='dashdot', marker='s', markersize=power_marker_size, label="H100 power", color='#4dc46d', markeredgecolor='#000000')#'#92f0ab')#'#f5dc84')
 
 
 # Plot throughput data
-ax.plot(xticks, cgen_u280, linestyle='-', marker='o',markersize=12, label='c_u280', color=colors[1])
-ax.plot(xticks, hand_u280, linestyle='--', marker='o', markersize=12, markerfacecolor='white', label='h_u280', color=colors[0])
-ax.plot(xticks, cgen_vck5000, linestyle='-', marker='d',markersize=12, label='c_vck5000', color=colors[3])
-ax.plot(xticks, hand_vck5000, linestyle='--', marker='d',markersize=12, markerfacecolor='white', label='h_vck5000', color=colors[2])
-ax.plot(xticks, h100_1b, linestyle='-', marker='^',markersize=12, label='h100_1b', color=colors[4])
-ax.plot(xticks, h100_100b, linestyle='-', marker='^',markersize=12, label='h100_100b', color=colors[5])
+ax.plot(xticks, cgen_u280, linestyle='-', marker='o',markersize=throughput_marker_size, label='c_U280', color=colors[1])
+ax.plot(xticks, hand_u280, linestyle='--', marker='o', markersize=throughput_marker_size, markerfacecolor='white', label='h_U280', color=colors[0])
+ax.plot(xticks, cgen_vck5000, linestyle='-', marker='d',markersize=throughput_marker_size, label='c_VCK', color=colors[3])
+ax.plot(xticks, hand_vck5000, linestyle='--', marker='d',markersize=throughput_marker_size, markerfacecolor='white', label='h_VCK', color=colors[2])
+ax.plot(xticks, h100_1b, linestyle='-', marker='^',markersize=throughput_marker_size, label='H100_1B', color=colors[4])
+ax.plot(xticks, h100_100b, linestyle='-', marker='^',markersize=throughput_marker_size, label='H100_100B', color=colors[5])
 
 
 #anotate power numbers
@@ -59,9 +59,9 @@ ax2.yaxis.set_major_formatter(FormatStrFormatter('%.0f'))
 
 # Add grid, labels, and legends
 ax.grid(which='both', axis='y', linewidth=1, alpha=0.5)
-ax.set_xlabel('Mesh Size')
-ax.set_ylabel('Throughput (GFLOP/s)')
-ax2.set_ylabel('Power Usage (KJ)')
+ax.set_xlabel('Mesh Size', fontsize=label_font_size)
+ax.set_ylabel('Throughput (GFLOP/s)', fontsize=label_font_size)
+ax2.set_ylabel('Power Usage - 1000B (KJ)', fontsize=label_font_size)
 ax.set_xticks(xticks)
 ax.set_xticklabels(xticks, rotation=0)
 
@@ -70,10 +70,10 @@ handles1, labels1 = ax.get_legend_handles_labels()
 handles2, labels2 = ax2.get_legend_handles_labels()
 handles = handles1 + handles2
 labels = labels1 + labels2
-ax.legend(handles, labels, loc=2, ncol=2, facecolor='w', framealpha=1, edgecolor='black', prop={'size': 12})
+ax.legend(handles, labels, loc=2, ncol=2, facecolor='w', framealpha=1, edgecolor='black', prop={'size': legends_font_size})
 
 # Set axis limits
-ax.set_ylim([0, 1000])
+ax.set_ylim([0, 1150])
 ax2.set_ylim([0, 45])
 
 # Save the figure
