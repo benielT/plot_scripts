48059ea396be468da929f05df93b5bc1971b8098
Beniel Thileepan <Beniel.Thileepan@warwick.ac.uk>
2025-03-11 22:12:53 +0300

    Updated barplots

--- ops_fpga/throughput_performance_comparision/Diffusion3D7pt_throughput_bar_stacked.py
+++ ops_fpga/throughput_performance_comparision/Diffusion3D7pt_throughput_bar_stacked.py
@@ -2,13 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.ticker import FormatStrFormatter
 import pandas as pd
-from lineplot_params import general_font_size, power_marker_size, throughput_marker_size, label_font_size, legends_font_size
+from barplot_params import general_font_size, power_marker_size, throughput_marker_size, label_font_size, \
+    legends_font_size, bar_width, iner_props, outer_props, energy_bar_gap, energy_bar_width, colors, bar_offset
 
 # Color Palette
 # colors = ['#003f5c', '#58508d', '#bc5090', '#ff6361', '#ffa600', '#34a853', '#ce87e6']
 # colors = ['#4e79a7', '#f28e2b', '#e15759', '#76b7b2', '#59a14f', '#edc949', '#af7aa1' ]
 # colors = ['#A6CEE3', '#1F78B4', '#B2DF8A', '#33A02C', '#FB9A99', '#E31A1C', '#FDBF6F']
-colors = ['#f05039', '#E57a77', '#eebab4', '#1f449c', '#3d65a5', '#7ca1cc', '#a8b6cc', '#8a5e00', '#ffc626', '#e6cf8e', '#5a626e', '#5c9e73']
+
 # Load data
 df = pd.read_csv("data/Diffusion3D7pt_throughput.csv")
 
@@ -33,39 +34,36 @@ plt.rcParams['hatch.color'] = colors[1]
 
 fig, ax = plt.subplots()
 
-bar_width = 0.15  # Adjust as needed
-energy_bar_width = 0.10
-energy_bar_gap = 0.12
 x_indexes = np.arange(len(xticks))
 
-iner_props = {'linestyle':'-', 'linewidth':'1.0'}
-outer_props = {'linestyle':'-', 'linewidth':'1.0'}
+bar_offset = 0.0
+ 
 # Plot throughput as bars
 plt.rcParams['hatch.color'] = colors[5]
-bar2 = ax.bar(x_indexes - 2*bar_width, cgen_u280, width=bar_width, label='c_U280', color=colors[5])
-bar2 = ax.bar(x_indexes - 2*bar_width, cgen_u280, width=bar_width, color='none', edgecolor='black', **iner_props)
+bar2 = ax.bar(x_indexes - bar_width + bar_offset, cgen_u280, width=bar_width, label='c_U280', color=colors[5])
+bar2 = ax.bar(x_indexes - bar_width + bar_offset, cgen_u280, width=bar_width, color='none', edgecolor='black', **iner_props)
 
 plt.rcParams['hatch.color'] = colors[5]
-bar1 = ax.bar(x_indexes - 2*bar_width, hand_u280, width=bar_width, label='h_U280', color='white', hatch='xxx')
-bar1 = ax.bar(x_indexes - 2*bar_width, hand_u280, width=bar_width, color='none', edgecolor='black', **iner_props)
+bar1 = ax.bar(x_indexes - bar_width + bar_offset, hand_u280, width=bar_width, label='h_U280', color='white', hatch='xxx')
+bar1 = ax.bar(x_indexes - bar_width + bar_offset, hand_u280, width=bar_width, color='none', edgecolor='black', **iner_props)
 
 
-bar5 = ax.bar(x_indexes - bar_width, cgen_vck5000, width=bar_width, label='c_VCK', color=colors[0])
-bar5 = ax.bar(x_indexes - bar_width, cgen_vck5000, width=bar_width, color='none', edgecolor='black', **iner_props)
+bar5 = ax.bar(x_indexes + bar_offset, cgen_vck5000, width=bar_width, label='c_VCK', color=colors[0])
+bar5 = ax.bar(x_indexes + bar_offset, cgen_vck5000, width=bar_width, color='none', edgecolor='black', **iner_props)
 
 plt.rcParams['hatch.color'] = colors[0]
-bar4 = ax.bar(x_indexes - bar_width, hand_vck5000, width=bar_width, label='h_VCK', color='white', hatch='///')
-bar4 = ax.bar(x_indexes - bar_width, hand_vck5000, width=bar_width, color='none', edgecolor='black', **iner_props)
+bar4 = ax.bar(x_indexes + bar_offset, hand_vck5000, width=bar_width, label='h_VCK', color='white', hatch='///')
+bar4 = ax.bar(x_indexes + bar_offset, hand_vck5000, width=bar_width, color='none', edgecolor='black', **iner_props)
 
-bar6 = ax.bar(x_indexes[:4], h100_10b[:4], width=bar_width, label='H100_10B', color=colors[9])
-bar6 = ax.bar(x_indexes[:4], h100_10b[:4], width=bar_width, color='none', edgecolor='black', **iner_props)
+bar6 = ax.bar(x_indexes[:4] + bar_width + bar_offset, h100_10b[:4], width=bar_width, label='H100_10B', color=colors[9])
+bar6 = ax.bar(x_indexes[:4] + bar_width + bar_offset, h100_10b[:4], width=bar_width, color='none', edgecolor='black', **iner_props)
 
 plt.rcParams['hatch.color'] = colors[9]
-bar7 = ax.bar(x_indexes, h100_1b, width=bar_width, label='H100_1B', color='white', hatch='---')
-bar7 = ax.bar(x_indexes, h100_1b, width=bar_width, color='none', edgecolor='black', **iner_props)
+bar7 = ax.bar(x_indexes + bar_width + bar_offset, h100_1b, width=bar_width, label='H100_1B', color='white', hatch='---')
+bar7 = ax.bar(x_indexes + bar_width + bar_offset, h100_1b, width=bar_width, color='none', edgecolor='black', **iner_props)
 
-bar6 = ax.bar(x_indexes[4:], h100_10b[4:], width=bar_width, color=colors[9])
-bar6 = ax.bar(x_indexes[4:], h100_10b[4:], width=bar_width, color='none', edgecolor='black', **iner_props)
+bar6 = ax.bar(x_indexes[4:] + bar_width + bar_offset, h100_10b[4:], width=bar_width, color=colors[9])
+bar6 = ax.bar(x_indexes[4:] + bar_width + bar_offset, h100_10b[4:], width=bar_width, color='none', edgecolor='black', **iner_props)
 
 
 # plt.rcParams['hatch.color'] = colors[4]
@@ -84,21 +82,23 @@ ax2 = ax.twinx()
 # ax2.plot(x_indexes, cgen_8192_u280_power, linestyle='dashdot', marker='^', markersize=power_marker_size, label="U280_8192 energy", color='#d9b3e6', markeredgecolor='#000000')
 # ax2.plot(x_indexes, h100_power, linestyle='dashdot', marker='^', markersize=power_marker_size, label="H100 energy", color='#4dc46d', markeredgecolor='#000000')
 
-plt.rcParams['hatch.color'] = colors[10]
-bar8 = ax2.bar(x_indexes[:4] + bar_width + energy_bar_gap, h100_1sys_power[:4], width=energy_bar_width, hatch="...", label='H100_1B energy', color='white')
-bar8 = ax2.bar(x_indexes[:4] + bar_width + energy_bar_gap, h100_1sys_power[:4], width=energy_bar_width, color='none', edgecolor='black', **outer_props)
+# plt.rcParams['hatch.color'] = colors[10]
+# bar8 = ax2.bar(x_indexes[:4] + bar_width + energy_bar_gap, h100_1sys_power[:4], width=energy_bar_width, hatch="...", label='H100_1B energy', color='white')
+# bar8 = ax2.bar(x_indexes[:4] + bar_width + energy_bar_gap, h100_1sys_power[:4], width=energy_bar_width, color='none', edgecolor='black', **outer_props)
 
 
-bar9 = ax2.bar(x_indexes + bar_width + energy_bar_gap, h100_10sys_power, width=energy_bar_width, label='H100_10B energy', color=colors[10])
-bar9 = ax2.bar(x_indexes + bar_width + energy_bar_gap, h100_10sys_power, width=energy_bar_width, color='none', edgecolor='black', **outer_props)
+# bar9 = ax2.bar(x_indexes + bar_width + energy_bar_gap, h100_10sys_power, width=energy_bar_width, label='H100_10B energy', color=colors[10])
+# bar9 = ax2.bar(x_indexes + bar_width + energy_bar_gap, h100_10sys_power, width=energy_bar_width, color='none', edgecolor='black', **outer_props)
 
-plt.rcParams['hatch.color'] = colors[10]
-bar8 = ax2.bar(x_indexes[4:] + bar_width + energy_bar_gap, h100_1sys_power[4:], width=energy_bar_width, hatch="...", color='white')
-bar8 = ax2.bar(x_indexes[4:] + bar_width + energy_bar_gap, h100_1sys_power[4:], width=energy_bar_width, color='none', edgecolor='black', **outer_props)
+# plt.rcParams['hatch.color'] = colors[10]
+# bar8 = ax2.bar(x_indexes[4:] + bar_width + energy_bar_gap, h100_1sys_power[4:], width=energy_bar_width, hatch="...", color='white')
+# bar8 = ax2.bar(x_indexes[4:] + bar_width + energy_bar_gap, h100_1sys_power[4:], width=energy_bar_width, color='none', edgecolor='black', **outer_props)
 
-plt.rcParams['hatch.color'] = colors[10]
-bar8 = ax2.bar(x_indexes + bar_width + energy_bar_gap, cgen_u280_power, width=energy_bar_width, hatch="\\\\\\", label='U280 energy', color='white')
-bar8 = ax2.bar(x_indexes + bar_width + energy_bar_gap, cgen_u280_power, width=energy_bar_width, color='none', edgecolor='black', **outer_props)
+# plt.rcParams['hatch.color'] = colors[10]
+# bar8 = ax2.bar(x_indexes + bar_width + energy_bar_gap, cgen_u280_power, width=energy_bar_width, hatch="\\\\\\", label='U280 energy', color='white')
+# bar8 = ax2.bar(x_indexes + bar_width + energy_bar_gap, cgen_u280_power, width=energy_bar_width, color='none', edgecolor='black', **outer_props)
+ax2.plot(x_indexes - bar_width, cgen_u280_power, linestyle='dashdot', marker='^', markersize=power_marker_size + 2, label="U280 energy", color='none', markerfacecolor='white', markeredgewidth=3.5, markeredgecolor=colors[12])
+ax2.plot(x_indexes  + bar_width, h100_1sys_power, linestyle='dashdot', marker='o', markersize=power_marker_size, label="H100 energy", color='none', markerfacecolor='white', markeredgewidth=3.5, markeredgecolor=colors[10])
 
 # Format the axes
 ax.yaxis.set_major_formatter(FormatStrFormatter('%.0f'))
@@ -120,7 +120,7 @@ labels = labels1 + labels2
 ax.legend(handles, labels, loc=2, ncol=2, facecolor='w', framealpha=1, edgecolor='black', prop={'size': 13})
 
 # Set axis limits
-ax.set_ylim([0, 1200])
+ax.set_ylim([0, 1000])
 ax2.set_ylim([0, 330])
 
 # Save the figure
