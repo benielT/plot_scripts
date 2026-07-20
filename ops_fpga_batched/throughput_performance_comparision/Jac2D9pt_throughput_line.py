9338f598d9ab05530dc93dd86a8313275ee10879
Beniel Thileepan <Beniel.Thileepan@warwick.ac.uk>
2026-07-21 01:46:13 +0200

    Added changes to jac2d batch throughput script

--- ops_fpga_batched/throughput_performance_comparision/Jac2D9pt_throughput_line.py
+++ ops_fpga_batched/throughput_performance_comparision/Jac2D9pt_throughput_line.py
@@ -12,7 +12,7 @@ df = pd.read_csv("../../ops_fpga/throughput_performance_comparision/data/Jac2D9p
 # Extract data from CSV
 xticks = batched_df["grid_size"]
 hand_u280 = df["handcoded_u280"]
-cgen_wo_batching_u280 = df["codegen_L_4096_H_360_u280"]
+cgen_wo_batching_u280 = df["codegen_u280"]
 cgen_b1_u280_loopback = batched_df["codegen_4096SLR_360HLS_LOOP_1B"]
 cgen_b1_u280_datcopy = batched_df["codegen_128SLR_10HLS_DATCPY_1B"]
 cgen_b10_u280_loopback = batched_df["codegen_4096SLR_360HLS_LOOP_10B"]
@@ -30,25 +30,25 @@ h100_100b = df["H100_100B"]
 # h100_power = df["pow_H100_1000B"]
 
 # Configure plot settings
-plt.rcParams["figure.figsize"] = (12, 7.5)
+plt.rcParams["figure.figsize"] = (9, 5.5)
 plt.rcParams.update({'font.size': general_font_size})
 
 fig, ax = plt.subplots()
 
 # Plot throughput data
-ax.plot(xticks, cgen_b1_u280_loopback, linestyle='-', marker='o', markersize=throughput_marker_size, label='c_1B_LB', color=colors[2])
-ax.plot(xticks, cgen_b10_u280_loopback, linestyle='-', marker='o', markersize=throughput_marker_size, label='c_10B_LB', color=colors[3])
-ax.plot(xticks, cgen_b20_u280_loopback, linestyle='-', marker='o', markersize=throughput_marker_size, label='c_20B_LB', color=colors[4])
-ax.plot(xticks, cgen_b50_u280_loopback, linestyle='-', marker='o', markersize=throughput_marker_size, label='c_50B_LB', color=colors[5])
-ax.plot(xticks, cgen_b1_u280_datcopy, linestyle='-', marker='d', markersize=throughput_marker_size, label='c_1B_CP', color=colors[6])
-ax.plot(xticks, cgen_b10_u280_datcopy, linestyle='-', marker='d', markersize=throughput_marker_size, label='c_10B_CP', color=colors[7])
-ax.plot(xticks, cgen_b20_u280_datcopy, linestyle='-', marker='d', markersize=throughput_marker_size, label='c_20B_CP', color=colors[8])
-ax.plot(xticks, cgen_b50_u280_datcopy, linestyle='-', marker='d', markersize=throughput_marker_size, label='c_50B_CP', color=colors[9])
-ax.plot(xticks, cgen_b100_u280_datcopy, linestyle='-', marker='d', markersize=throughput_marker_size, label='c_100B_CP', color=colors[10])
-ax.plot(xticks[2:], hand_u280, linestyle='--', marker='o', markersize=throughput_marker_size, markerfacecolor='white', label='h_U280', color=colors[0])
-ax.plot(xticks[2:], cgen_wo_batching_u280, linestyle='-', marker='o', markersize=throughput_marker_size, label='c_WOB_LB', color=colors[1])
-ax.plot(xticks[2:], h100_1b, linestyle='-', marker='^',markersize=throughput_marker_size, label='H100_1B', color=colors[11])
-ax.plot(xticks[2:], h100_100b, linestyle='-', marker='^',markersize=throughput_marker_size, label='H100_100B', color=colors[12])
+ax.plot(xticks[:-3], cgen_b1_u280_loopback[:-3], linestyle='-', marker='o', markersize=throughput_marker_size, label='c_1B_LB', color=colors[2])
+ax.plot(xticks[:-3], cgen_b10_u280_loopback[:-3], linestyle='-', marker='o', markersize=throughput_marker_size, label='c_10B_LB', color=colors[3])
+ax.plot(xticks[:-3], cgen_b20_u280_loopback[:-3], linestyle='-', marker='o', markersize=throughput_marker_size, label='c_20B_LB', color=colors[4])
+ax.plot(xticks[:-3], cgen_b50_u280_loopback[:-3], linestyle='-', marker='o', markersize=throughput_marker_size, label='c_50B_LB', color=colors[5])
+ax.plot(xticks[:-3], cgen_b1_u280_datcopy[:-3], linestyle='-', marker='d', markersize=throughput_marker_size, label='c_1B_CP', color=colors[6])
+ax.plot(xticks[:-3], cgen_b10_u280_datcopy[:-3], linestyle='-', marker='d', markersize=throughput_marker_size, label='c_10B_CP', color=colors[7])
+ax.plot(xticks[:-3], cgen_b20_u280_datcopy[:-3], linestyle='-', marker='d', markersize=throughput_marker_size, label='c_20B_CP', color=colors[8])
+ax.plot(xticks[:-3], cgen_b50_u280_datcopy[:-3], linestyle='-', marker='d', markersize=throughput_marker_size, label='c_50B_CP', color=colors[9])
+ax.plot(xticks[:-3], cgen_b100_u280_datcopy[:-3], linestyle='-', marker='d', markersize=throughput_marker_size, label='c_100B_CP', color=colors[10])
+ax.plot(xticks[2:-3], hand_u280[:-3], linestyle='--', marker='o', markersize=throughput_marker_size, markerfacecolor='white', label='h_U280', color=colors[0])
+ax.plot(xticks[2:-3], cgen_wo_batching_u280[:-3], linestyle='-', marker='o', markersize=throughput_marker_size, label='c_WOB_LB', color=colors[1])
+ax.plot(xticks[2:-3], h100_1b[:-3], linestyle='-', marker='^',markersize=throughput_marker_size, label='H100_1B', color=colors[11])
+ax.plot(xticks[2:-3], h100_100b[:-3], linestyle='-', marker='^',markersize=throughput_marker_size, label='H100_100B', color=colors[12])
 
 # Add secondary y-axis for power usage
 # ax2 = ax.twinx()
@@ -65,8 +65,8 @@ ax.grid(which='both', axis='y', linewidth=1, alpha=0.5)
 ax.set_xlabel('Mesh Size', fontsize=label_font_size)
 ax.set_ylabel('Throughput (GFLOP/s)', fontsize=label_font_size)
 # ax2.set_ylabel('Energy: 1k Batches (kJ)', fontsize=label_font_size)
-ax.set_xticks(xticks)
-ax.set_xticklabels(xticks, rotation=0)
+ax.set_xticks(xticks[:-3])
+ax.set_xticklabels(xticks[:-3], rotation=0)
 
 # Combine legends from both axes
 handles1, labels1 = ax.get_legend_handles_labels()
@@ -76,7 +76,7 @@ labels = labels1 #+ labels2
 ax.legend(handles, labels, loc=2, ncol=4, facecolor='w', framealpha=1, edgecolor='black', prop={'size': 13})
 
 # Set axis limits
-ax.set_ylim([0, 1700])
+ax.set_ylim([0, 1800])
 # ax2.set_ylim([0, 50])
 
 # Save the figure
